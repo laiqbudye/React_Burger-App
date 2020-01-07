95385b1ef182f9662b6e9b65544735e3d9caeb6a
laiqb <laiqbudye@gmail.com>
2020-01-08 01:05:59 +0530

    BuildControls || added component which will create series of buttons "Less", "More" buttons

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import BuildControl from './BuildControl/BuildControl';
+import './BuildControls.css';
+
+const controls = [
+    { label: 'Chicken', type: 'chicken' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
+    { label: 'Salad', type: 'salad' }
+];
+
+
+
+const BuildControls = (props) => (
+    <div className='BuildControls'>
+        <p>Current price is: <strong>{props.price.toFixed(2)}</strong></p>
+        {controls.map(ctrl =>
+            <BuildControl
+                key={ctrl.label}
+                label={ctrl.label}
+                added={() => { props.ingredientadded(ctrl.type) }}     // passing type of food to addingredient
+                removed={() => { props.ingredientremoved(ctrl.type) }}
+                disabled={props.disabled[ctrl.type]} />
+        )}
+
+    </div>
+);
+
+export default BuildControls;
