4268cae30ddbc64ae31cdb7b7c084ab64e606088
laiqb <laiqbudye@gmail.com>
2020-01-08 01:02:13 +0530

    BurgerBuilder || adding component

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import Auxiliary from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+    salad: 5,
+    chicken: 10,
+    cheese: 10,
+    meat: 15
+};
+
+class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salad: 0,
+            chicken: 0,
+            cheese: 0,
+            meat: 0
+        },
+        totalprice: 10
+    }
+
+    addIngredientHandler = (type) => {
+        const oldcount = this.state.ingredients[type];
+        const updatedcount = oldcount + 1;
+        const updatedIngredients = { ...this.state.ingredients };
+        updatedIngredients[type] = updatedcount;
+
+        const baseprice = this.state.totalprice;
+        const priceaddition = INGREDIENT_PRICES[type];
+        const newprice = baseprice + priceaddition;
+        this.setState({ totalprice: newprice, ingredients: updatedIngredients })
+    };
+
+    removeIngredienthandler = (type) => {
+        const oldcount = this.state.ingredients[type];
+
+        if (oldcount <= 0) {
+            return;      //fixed issue if we press less button before adding something
+        }
+        const updatedcount = oldcount - 1;
+        const updatedIngredients = { ...this.state.ingredients };
+        updatedIngredients[type] = updatedcount;
+
+        const baseprice = this.state.totalprice;
+        const pricededuction = INGREDIENT_PRICES[type];
+        const newprice = baseprice - pricededuction;
+        this.setState({ totalprice: newprice, ingredients: updatedIngredients })
+    };
+
+    render() {
+        const disabledInfo = { ...this.state.ingredients };
+
+        for (const key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
+
+            //return object as {salad: true, meat: false, cheese: true ....}
+        }
+        return (
+            <Auxiliary>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls ingredientadded={this.addIngredientHandler}
+                    ingredientremoved={this.removeIngredienthandler}
+                    disabled={disabledInfo}
+                    price= {this.state.totalprice} />
+            </Auxiliary>
+        )
+    }
+}
+
+export default BurgerBuilder;
