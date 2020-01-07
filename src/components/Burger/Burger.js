a1a66c8984cee219d1206481a851b55dc10511a5
laiqb <laiqbudye@gmail.com>
2020-01-08 01:02:37 +0530

    Burger || adding component

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import BurgerIngredient from '../Burger/BurgerIngredient/BurgerIngredient';
+import './Burger.css';
+
+const burger = (props) => {
+
+    let transformedIngredients = Object.keys(props.ingredients)
+    .map(igkey => {
+        console.log(igkey)
+        return [...Array(props.ingredients[igkey])].map((_,i)=>{
+            console.log(igkey + i)
+            return <BurgerIngredient key = {igkey + i} type = {igkey} />
+        })
+    }).reduce((arr,el) => {
+        return arr.concat(el);
+    }, []);
+
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingredients</p>
+    }
+    return (
+        <div className='Burger'>
+            <BurgerIngredient type='bread-top'/>
+            {transformedIngredients}
+            <BurgerIngredient type='bread-bottom'/>
+        </div>
+    )
+}
+
+export default burger;
