d7fb1aabf4a4e676f075d47a43a6a6adbd53351f
laiqb <laiqbudye@gmail.com>
2020-01-11 04:54:05 +0530

    Layout || side Drawer || modified Layout component from functional component to class component as we need to maintain state in that.. to provide state to side drawer component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import './Layout.css';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Auxiliary>
-        <Toolbar/>
-        <SideDrawer/>
-        <main className ='mainarea'>
-            {props.children}
-        </main>
-    </Auxiliary>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
 
-export default layout;
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false });
+    }
+
+    sidedrawerTogglehandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer}
+        })
+    }
+
+    render() {
+        return (
+            <Auxiliary>
+                <Toolbar toggler = {this.sidedrawerTogglehandler}/>
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler}>
+                </SideDrawer>
+                <main className='mainarea'>
+                    {this.props.children}
+                </main>
+            </Auxiliary>
+        );
+    }
+}
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,15 +2,28 @@ import React from 'react';
 import './SideDrawer.css';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import Logo from '../../Logo/Logo';
+import Aux from '../../../hoc/Auxiliary';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
-const SideDrawer = () => {
-    return(
-    <div className='SideDrawer'>
-        <div className='Logo_2'><Logo/></div>
-        <nav>
-            <NavigationItems/>
-        </nav>
-    </div>
+const SideDrawer = (props) => {
+let attachedClasses = ['SideDrawer','Close'];
+console.log("aaa " +props.open );
+
+if(props.open){
+    attachedClasses = ['SideDrawer','Open'];
+}
+    return (
+        <Aux>
+            <Backdrop
+                show={props.open}
+                backdropclicked={props.closed} ></Backdrop>
+            <div className= {attachedClasses.join(' ')}>
+                <div className='Logo_2'><Logo /></div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
     )
 };
 
